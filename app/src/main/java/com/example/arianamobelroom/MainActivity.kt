84b9ac2f58020075b96a52b1c96c6d7136bbf0d7
CrossAxis98 <roberto2611@gmail.com>
2022-09-05 19:07:34 +0200

    Add appScreen2 :photo:

--- app/src/main/java/com/example/arianamobelroom/MainActivity.kt
+++ app/src/main/java/com/example/arianamobelroom/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -17,6 +18,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
@@ -188,6 +190,7 @@ fun MobelRow(index: Int, item: Mobel, onMobelClicked: (Mobel) -> Unit) {
     val color = RandomColors().getColor()
     Card(modifier = Modifier
         .padding(8.dp)
+        .clip(RoundedCornerShape(topEnd = 33.dp))
         .fillMaxWidth(), elevation = 4.dp,
     backgroundColor = Color(color)) {
         Row(
